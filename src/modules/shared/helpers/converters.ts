9db87322e45fa14a0d43d056b76beaaeef3d6178
Iván Guerrero <ivanguebri@gmail.com>
2022-01-27 10:28:20 -0500

    Complete user stories and bonus features.
    Yet some acceptable UI is still missing.

--- src/modules/shared/helpers/converters.ts
+++ src/modules/shared/helpers/converters.ts
@@ -1,3 +1,14 @@
-export const convertToDecimal = (binaryValue: string) => {
-  return binaryValue;
+// this function conforms to the constraint that the decimal equivalent of
+// a binary digit must be calculated with a function
+const digitToDecimalEquivalent = (digit: number, power: number): number =>
+  digit * Math.pow(2, power);
+
+export const convertToDecimal = (binaryValue: string): string => {
+  let decValue = 0;
+  const binaryValueLength = binaryValue.length;
+  for (let idx = 0; idx < binaryValueLength; idx++) {
+    const power = binaryValueLength - idx - 1;
+    decValue += digitToDecimalEquivalent(Number(binaryValue[idx]), power);
+  }
+  return `${decValue}`;
 };
